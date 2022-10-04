59caec4b2f2fba751cdb8b31f15934e40b0f92bc
solbae <sol4854@naver.com>
2022-10-04 15:48:23 +0900

    First CodeUp 문제

--- Java/src/even_odd_plus/Main.java
+++ Java/src/even_odd_plus/Main.java
@@ -3,28 +3,42 @@ package even_odd_plus;
 import java.util.Scanner;
 
 public class Main {
+    public static String getEvenOdd(int num){
+        if (num % 2 == 0)
+            return "짝수";
+        else
+            return "홀수";
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String[] str = new String[] {"짝수", "홀수"};
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int firstNum = sc.nextInt();
+        int secNum = sc.nextInt();
 
-        if(a % 2 == 0) {
-            if (b % 2 == 0)
-                System.out.print(str[0] + "+" + str[0]);
-            else
-                System.out.print(str[0] + "+" + str[1]);
-        } else {
-            if (b % 2 == 0)
-                System.out.print(str[1] + "+" + str[0]);
-            else
-                System.out.print(str[1] + "+" + str[1]);
-        }
 
-        if ((a + b) % 2 == 0)
-            System.out.println("=" + str[0]);
-        else
-            System.out.println("=" + str[1]);
+        String strFist = getEvenOdd(firstNum);
+        String strSec = getEvenOdd(secNum);
+        String strSum = getEvenOdd(firstNum + secNum);
+
+        System.out.printf("%s+%s=%s", strFist,strSec,strSum);
+
+//        if(firstNum % 2 == 0) {
+//            if (secNum % 2 == 0)
+//                System.out.print(str[0] + "+" + str[0]);
+//            else
+//                System.out.print(str[0] + "+" + str[1]);
+//        } else {
+//            if (secNum % 2 == 0)
+//                System.out.print(str[1] + "+" + str[0]);
+//            else
+//                System.out.print(str[1] + "+" + str[1]);
+//        }
+//
+//        if (sumNum % 2 == 0)
+//            System.out.println("=" + str[0]);
+//        else
+//            System.out.println("=" + str[1]);
    }
 }
